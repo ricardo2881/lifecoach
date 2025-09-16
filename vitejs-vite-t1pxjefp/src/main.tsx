6ca1dca62cca6bfb81c7393776f0121f68dacb09
ricardo2881 <ricardovivas2881@gmail.com>
2025-09-16 19:51:02 +1000

    Update main.tsx

--- vitejs-vite-t1pxjefp/src/main.tsx
+++ vitejs-vite-t1pxjefp/src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link, Outlet } from 'react-router-dom';
-import App from './App';
 import Weekly from './routes/Weekly';
 
 function Shell() {
   return (
-    // ONE parent element
     <div>
       <nav style={{ padding: 12, borderBottom: '1px solid #eee' }}>
-        <Link to="/" style={{ marginRight: 12 }}>Dashboard</Link>
-        <Link to="/weekly">Weekly</Link>
+        <Link to="/weekly" style={{ marginRight: 12, fontWeight: 600 }}>Weekly Focus</Link>
+        <Link to="/" style={{ opacity: 0.7 }}>Home</Link>
       </nav>
       <Outlet />
     </div>
@@ -21,7 +19,8 @@ const router = createBrowserRouter([
   {
     element: <Shell />,
     children: [
-      { path: '/', element: <App /> },
+      // Show Weekly at the home page too (so you can't miss it)
+      { path: '/', element: <Weekly /> },
       { path: '/weekly', element: <Weekly /> }
     ]
   }
