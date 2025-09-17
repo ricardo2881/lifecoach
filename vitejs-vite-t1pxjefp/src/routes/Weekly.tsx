52aae51ec43f0e495f88356676a21b9a6715f356
ricardo2881 <ricardovivas2881@gmail.com>
2025-09-17 15:59:44 +1000

    Update Weekly.tsx

--- vitejs-vite-t1pxjefp/src/routes/Weekly.tsx
+++ vitejs-vite-t1pxjefp/src/routes/Weekly.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export default function Weekly() {
   return (
     <div style={{ padding: 16 }}>
       <h1 style={{ fontSize: 24, fontWeight: 600 }}>Weekly Focus</h1>
-      <p style={{ opacity: 0.7 }}>
-        If you can see this, the route is wired correctly.
-      </p>
-      <p style={{ marginTop: 12 }}>
-        Next we can re-enable data (Dexie) and the timer.
-      </p>
-      <a href="/#/" style={{ display: 'inline-block', marginTop: 16, textDecoration: 'underline' }}>
-        Back to Home
-      </a>
+      <p style={{ opacity: 0.7 }}>If you can read this, routing works.</p>
     </div>
   );
 }
